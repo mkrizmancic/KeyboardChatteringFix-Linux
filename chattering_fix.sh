98ccc14b41f9578fa0fff179983e87def0ddee09
Marko Krizmancic <marko.krizmancic@fer.hr>
2025-02-05 13:56:39 +0100

    Add config for my Logitech G413

--- chattering_fix.sh
+++ chattering_fix.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 # Change the line below to the absolute path of the folder
-cd <absolute/path/to/folder> && sudo python3 -m src -k <KEYBOARD id> -t <OPTIONAL, 30 is the default threshold>
+cd /home/marko/ThirdParty/KeyboardChatteringFix-Linux && sudo python3 -m src -k usb-Logitech_G413_Carbon_Mechanical_Gaming_Keyboard_046538603638-event-kbd
